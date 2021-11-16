cf70b6e496811d7d0c49be15af396e7962637c5a
kafanovn <92315991+kafanovn@users.noreply.github.com>
2021-11-16 11:16:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
+using System.IO;
+
 
 namespace ConsoleApp1
 {
@@ -25,10 +29,10 @@ namespace ConsoleApp1
     }
 
 
-    abstract class V4Data
+    abstract class V4Data: IEnumerable<DataItem>
     {
-        public string obj { get; }
-        public DateTime data { get; }
+        public string obj { get; protected set; }
+        public DateTime data { get; protected set; }
 
         public V4Data(string obj, DateTime data)
         {
@@ -39,6 +43,16 @@ namespace ConsoleApp1
         public abstract float MaxFromOrigin { get; }
         public abstract string ToLongString(string format);
         public abstract override string ToString();
+        protected abstract IEnumerator GetEnumerator_();
+        protected abstract IEnumerator<DataItem> GetEnumerator_DataItem();
+        IEnumerator<DataItem> IEnumerable<DataItem>.GetEnumerator()
+        {
+            return GetEnumerator_DataItem();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator_();
+        }
     }
 
 
@@ -118,15 +132,70 @@ namespace ConsoleApp1
             return String.Format("ClassName:{0}\n obj:{1}\n data:{2}\n Count:{3} coords:\n", "V4DataList", obj, data, this.Count) + '\n';
         }
 
+        protected override IEnumerator GetEnumerator_()
+        {
+            return DataList.GetEnumerator();
+        }
+        protected override IEnumerator<DataItem> GetEnumerator_DataItem()
+        {
+            return DataList.GetEnumerator();
+        }
+        public bool SaveAsText(string filename)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(filename))
+                {
+                    sw.WriteLine(obj);
+                    sw.WriteLine(data);
+                    sw.WriteLine(Count);
+                    foreach (var item in DataList)
+                    {
+                        sw.WriteLine(item.place.X);
+                        sw.WriteLine(item.place.Y);
+                        sw.WriteLine(item.value.X);
+                        sw.WriteLine(item.value.Y);
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool LoadAsText(string filename)
+        {
+            try
+            {
+                using (var sr = new StreamReader(filename))
+                {
+                    obj = sr.ReadLine();
+                    data = DateTime.Parse(sr.ReadLine());
+                    int count = int.Parse(sr.ReadLine());
+                    for (int i = 0; i < count; i++)
+                    {
+                        DataItem newitem = new DataItem(new Vector2(float.Parse(sr.ReadLine()), float.Parse(sr.ReadLine())), new Vector2(float.Parse(sr.ReadLine()), float.Parse(sr.ReadLine())));
+                        this.Add(newitem);
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
     }
 
 
     class V4DataArray : V4Data
     {
-        public int xSteps { get; }
-        public int ySteps { get; }
-        public Vector2 Step { get; }
-        public Vector2[,] array { get; }
+        public int xSteps { get; private set; }
+        public int ySteps { get; private set; }
+        public Vector2 Step { get; private set; }
+        public Vector2[,] array { get; private set; }
 
         public V4DataArray(string obj, DateTime data) : base(obj, data)
         {
@@ -207,6 +276,67 @@ namespace ConsoleApp1
             }
             return DataList;
         }
+        protected override IEnumerator GetEnumerator_()
+        {
+            return this.ArrayToList().DataList.GetEnumerator();
+        }
+        protected override IEnumerator<DataItem> GetEnumerator_DataItem()
+        {
+            return this.ArrayToList().DataList.GetEnumerator();
+        }
+
+        public bool SaveBinary(string filename)
+        {
+            try
+            {
+                using (var bw = new BinaryWriter(File.Open(filename, FileMode.Create)))
+                {
+                    bw.Write(obj);
+                    bw.Write(data.ToBinary());
+                    bw.Write(xSteps);
+                    bw.Write(ySteps);
+                    bw.Write(Step.X);
+                    bw.Write(Step.Y);
+                    foreach (var vec in array)
+                    {
+                        bw.Write(vec.X);
+                        bw.Write(vec.Y);
+                    };
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool LoadBinary(string filename)
+        {
+            try
+            {
+                using (var br = new BinaryReader(File.Open(filename, FileMode.Open)))
+                {
+                    obj = br.ReadString();
+                    data = DateTime.FromBinary(br.ReadInt64());
+                    xSteps = br.ReadInt32();
+                    ySteps = br.ReadInt32();
+                    Step = new Vector2(br.ReadSingle(), br.ReadSingle());
+                    array = new Vector2[xSteps, ySteps];
+                    for (var i = 0; i < xSteps; i++)
+                    {
+                        for (var j = 0; j < ySteps; j++)
+                        {
+                            array[i, j] = new Vector2(br.ReadSingle(), br.ReadSingle());
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
     }
 
     class V4MainCollection
@@ -252,6 +382,43 @@ namespace ConsoleApp1
             }
             return str;
         }
+        public float MaxFromArrays
+        {
+           get
+            {
+                var items = from i in Collection where i is V4DataArray from j in i select j;
+                var Field_vals = from i in items select i.value.Length();
+                if (Field_vals != null && Field_vals.Any())
+                    return  Field_vals.Max();
+                else
+                    return float.NaN;
+            }
+        }
+        public IEnumerable<DataItem> DataCoords
+        {
+            get
+            {
+                var items = from i in Collection from j in i select j;
+                var result = from i in items orderby i.place.Length() descending select i;
+                if (result != null && result.Any())
+                    return result;
+                else
+                    return null;
+            }
+        }
+        public IEnumerable<Vector2> OnlyArray
+        {
+            get
+            {
+                var numVector2List = from i in Collection where i is V4DataList from j in i select j.place;
+                var numVector2Array = from i in Collection where i is V4DataArray from j in i select j.place;
+                var result = numVector2Array.Except<Vector2>(numVector2List);
+                if (result != null && result.Any())
+                    return result;
+                else
+                    return null;
+            }
+        }
     }
 
     static class Fields
@@ -263,57 +430,82 @@ namespace ConsoleApp1
     }
     class Program
     {
-        static void Main(string[] args)
-        {/*
-            FvVector2 F = Fields.E;
-            V4DataArray arr = new V4DataArray("V4DateArray", new DateTime(1, 1, 1), 1, 2, new Vector2(1, 0.11f), F);
-            Console.WriteLine(arr.ToLongString("{0:f2} {1:f2} \n"));
-            V4DataList list = arr.ArrayToList();
-            Console.WriteLine(list.ToLongString("{0:f2} {1:f2} \n"));
-            Console.WriteLine(" ArrCount: {0}\n ArrMaxFromOrigin: {1:f2}\n ListCount: {2}\n ListMaxFromOrigin: {3:f2}\n", arr.Count, arr.MaxFromOrigin, list.Count, list.MaxFromOrigin);
-         
-            V4MainCollection collection = new V4MainCollection();
-            collection.Add(list);
-            collection.Add(arr);
-            
-
-            V4DataArray arr1 = new V4DataArray("V4DateArray", new DateTime(1, 1, 1), 0, 2, new Vector2(1, 0.11f), F);
-            collection.Add(arr1);
-            V4DataList list1 = new V4DataList("List1", DateTime.Now);
-            list1.AddDefaults(2, F);
-            collection.Add(list1);
-            collection.Add(new V4DataList("emptyList", DateTime.Now));
-
-            for (int i = 0; i < collection.Count(); i++)
-            {
-                Console.WriteLine("Count {0:f2} MaxFromOrigin {1:f2}", collection[i].Count, collection[i].MaxFromOrigin);
-            }*/
-           
-
-            FvVector2 F = Fields.E;
-            V4DataArray arr = new V4DataArray("V4DateArray", new DateTime(1, 1, 1), 1, 2, new Vector2(1, 0.11f), F);
+        static void FileTest(string filename)
+        {
+            V4DataArray arr = new V4DataArray("test", DateTime.Today, 2, 2, new Vector2(1, 0.11f), Fields.E);
+            arr.SaveBinary(filename);
             Console.WriteLine(arr.ToLongString("f2"));
-            V4DataList list = arr.ArrayToList();
+            V4DataArray copy_arr = new V4DataArray("test", DateTime.Today);
+            copy_arr.LoadBinary(filename);
+            Console.WriteLine(arr.ToLongString("f2"));
+
+            V4DataList list = new V4DataList("test", DateTime.Today);
+            list.AddDefaults(3, Fields.E);
+            list.SaveAsText(filename);
             Console.WriteLine(list.ToLongString("f2"));
-            Console.WriteLine(" ArrCount: {0}\n ArrMaxFromOrigin: {1:f2}\n ListCount: {2}\n ListMaxFromOrigin: {3:f2}\n", arr.Count, arr.MaxFromOrigin, list.Count, list.MaxFromOrigin);
+            V4DataList copy_list = new V4DataList("test", DateTime.Today);
+            copy_list.LoadAsText(filename);
+            Console.WriteLine(copy_list.ToLongString("f2"));
 
-            DataItem dataitem = new DataItem(new Vector2(1.1f, 2.2f), new Vector2(7.7f, 9.9f));
-            Console.WriteLine($"DataItem\n {dataitem.ToLongString("F2")}\n");
+            Console.WriteLine("Сохраняем и востанавливаем пустой массив");
+            V4DataArray arr1 = new V4DataArray("test1", DateTime.Today, 0, 2, new Vector2(1, 0.11f), Fields.E);
+            arr1.SaveBinary(filename);
+            Console.WriteLine(arr1.ToLongString("f2"));
+            V4DataArray copy_arr1 = new V4DataArray("test", DateTime.Today);
+            copy_arr1.LoadBinary(filename);
+            Console.WriteLine(arr1.ToLongString("f2"));
 
-            V4MainCollection collection = new V4MainCollection();
-
-            V4DataArray arr1 = new V4DataArray("V4DateArray", new DateTime(1, 1, 1), 0, 2, new Vector2(1, 0.11f), F);
-            collection.Add(arr1);
-            V4DataList list1 = new V4DataList("List1", DateTime.Now);
-            list1.AddDefaults(2, F);
-            collection.Add(list1);
-            collection.Add(new V4DataList("emptyList", DateTime.Now));
-
-            Console.WriteLine(collection.ToLongString("f2"));
-            for (int i = 0; i < collection.Count(); i++)
+        }
+        static void LINQ()
+        {
+            try
             {
-                Console.WriteLine("Count {0:f2} MaxFromOrigin {1:f2}", collection[i].Count, collection[i].MaxFromOrigin);
+                V4MainCollection collection = new V4MainCollection();
+                collection.Add(new V4DataArray("test1", DateTime.Today, 2, 2, new Vector2(1, 0.11f), Fields.E));
+                collection.Add(new V4DataArray("test2", DateTime.Today, 0, 2, new Vector2(0.2f, 0.5f), Fields.E));
+                V4DataList testList = new V4DataList("test3", DateTime.Today);
+                testList.AddDefaults(0, Fields.E);
+                collection.Add(testList);
+                V4DataList testList2 = new V4DataList("test4", DateTime.Today);
+                testList2.AddDefaults(3, Fields.E);
+                collection.Add(testList2);
+                Console.WriteLine(collection.ToLongString("f2"));
+                Console.WriteLine("Максимальный модуль поля среди массивов: ");
+                Console.WriteLine(collection.MaxFromArrays);
+                Console.WriteLine("Точки поля содержащиеся только в V4DataArray: ");
+                foreach (var i in collection.DataCoords)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("Точки которые содержатся в V4DataArray, но не содержатся в V4DataList: ");
+                foreach (var i in collection.OnlyArray)
+                {
+                    Console.WriteLine(i.X);
+                    Console.WriteLine(i.Y);
+                }
+                V4MainCollection collection1 = new V4MainCollection();
+                foreach (var i in collection1.DataCoords)
+                {
+                    Console.WriteLine(i);
+                }
+                foreach (var i in collection1.OnlyArray)
+                {
+                    Console.WriteLine(i.X);
+                    Console.WriteLine(i.Y);
+                }
             }
+            catch(System.NullReferenceException)
+            {
+                Console.WriteLine("Попытка вызвать методы пустой коллекции");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Проверка записи и чтения");
+            FileTest("file.txt");
+            Console.WriteLine("Проверка запросов");
+            LINQ();
         }
     }
 }
